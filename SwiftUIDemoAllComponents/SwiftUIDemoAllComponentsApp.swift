468f7e56a87fab750b4593c1de49d3dd9cd876fd
anand NewVision <anand.mandal@newvision-software.com>
2026-05-20 11:53:08 +0530

    Initial Commit

---
+++ SwiftUIDemoAllComponents/SwiftUIDemoAllComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoAllComponentsApp.swift
+//  SwiftUIDemoAllComponents
+//
+//  Created by NewVision on 20/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoAllComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIDemoAllComponentsTests/SwiftUIDemoAllComponentsTests.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIDemoAllComponentsTests.swift
+//  SwiftUIDemoAllComponentsTests
+//
+//  Created by NewVision on 20/05/26.
+//
+
+import Testing
+@testable import SwiftUIDemoAllComponents
+
+struct SwiftUIDemoAllComponentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
